91109b00f1b1dbcfe34e564f72b106824f0b9bd7
VisvShah <visvshah1@gmail.com>
2023-01-21 00:43:59 -0500

    Auth Update Setup

--- front-end/src/Components/Auth/auth.jsx
+++ front-end/src/Components/Auth/auth.jsx
@@ -3,6 +3,24 @@ import "./auth.css"
 import {useNavigate} from "react-router-dom";
 
 export default function Auth() {
+    const [currentPage, changePage] = useState(0);
+    const moveLeft = ()=>{
+        if(currentPage>0) {
+            changePage(currentPage-1);
+        }
+        else {
+            currentPage(2);
+        }
+        
+    }
+    const moveRight = ()=>{
+        if(currentPage<2) {
+            changePage(currentPage+1);
+        }
+        else {
+            changePage(0);
+        }
+    }
     const [logIn, changeLogIn] = useState(false);
     const [error, setError] = useState(false);
     const navigate = useNavigate();
@@ -89,30 +107,38 @@ export default function Auth() {
     })
 
 return (
-    <div>
-        {/*<div className="authPage">
-            <h1 className = "header" >{logIn ? "Log In" : "Sign Up"}</h1>
+        <div className="authPage">
             <form autoComplete = "off" validate = "true" className = "form" onSubmit = {handleSubmit}> 
-                <input placeholder = "Your email" id = "email" name = "email" type ="email" onChange = {(e) => settutorData({...tutorData, email: e.target.value})}/>
-                { !logIn && (
-                    <>
-                        <input placeholder = "Your first name" id = "fName" name = "fName" type ="fName" onChange = {(e) => settutorData({...tutorData, fName: e.target.value})}/>
-                        <input placeholder = "Your last name" id = "lName" name = "lName" type ="lName" onChange = {(e) => settutorData({...tutorData, lName: e.target.value})}/>
-                        <input placeholder = "Your age" id = "age" name = "age" type ="age" onChange = {(e) => settutorData({...tutorData, age: e.target.value})}/>
-                    </>
-                    )
-                }
+                <div className="projectSlider" style = {{transform: `translateX(-${currentPage * 100}vw)`}}>
+                    <div className="projectHolder">
+                        <h1 className = "header" >{logIn ? "Log In" : "Sign Up"}</h1>
+                        
+                        <input placeholder = "Your email" id = "email" name = "email" type ="email" onChange = {(e) => settutorData({...tutorData, email: e.target.value})}/>
+                        { !logIn && (
+                            <>
+                                <input placeholder = "Your first name" id = "fName" name = "fName" type ="fName" onChange = {(e) => settutorData({...tutorData, fName: e.target.value})}/>
+                                <input placeholder = "Your last name" id = "lName" name = "lName" type ="lName" onChange = {(e) => settutorData({...tutorData, lName: e.target.value})}/>
+                                <input placeholder = "Your age" id = "age" name = "age" type ="age" onChange = {(e) => settutorData({...tutorData, age: e.target.value})}/>
+                            </>
+                            )
+                        }
+                        
+                        <input placeholder = "Enter Password" id = "password" name = "password" type ="password" onChange = {(e) => settutorData({...tutorData, password: e.target.value})}/>
+                        { logIn && (
+                            <button className = "submitButton" type="submit" onClick = {handleSubmit}>Submit</button>
+                        )}
+                        {error && (
+                            <>
+                                <div className = "error">Error!</div>
+                            </>
+                        )}
                 
-                <input placeholder = "Enter Password" id = "password" name = "password" type ="password" onChange = {(e) => settutorData({...tutorData, password: e.target.value})}/>
-                <button className = "submitButton" type="submit" onClick = {handleSubmit}>Submit</button>
-                {error && (
-                    <>
-                        <div className = "error">Error!</div>
-                    </>
-                )}
+                    <button className = "changeMode" onClick = {changeMode}>{logIn ? "Sign Up Instead" : "Log In instead"}</button>
+                    </div>
+                </div>
             </form>
-            <button className = "changeMode" onClick = {changeMode}>{logIn ? "Sign Up Instead" : "Log In instead"}</button>
-        </div>*/}
-    </div>
+            <button onClick = {()=>moveLeft()} className='leftArrow'>Back</button>
+            <button onClick = {()=>moveRight()} className = 'rightArrow'>Next</button>
+        </div>
     )
 }
