fea69b9a58460ce10fd600d369f7b6940eee9e60
Soham Jog <jogsoham2003@gmail.com>
2023-01-21 16:19:53 -0500

    fixing tutoravailcontroller

--- back-end/Routes/tutorAvailRoutes.js
+++ back-end/Routes/tutorAvailRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 const router = express.Router()
-import { createTutorAvail, fetchTutors, requestTutor, createSession, createReview} from '../Controllers/tutorAvailController.js'
+import { createTutorAvail, fetchTutors, requestTutor, createSession, createReview, deleteAvailTutor} from '../Controllers/tutorAvailController.js'
 
 router.post('/create', createTutorAvail);
 router.patch('/fetch', fetchTutors);
 router.patch('/request', requestTutor);
-
+router.delete('/:id', deleteAvailTutor)
 
 
 
