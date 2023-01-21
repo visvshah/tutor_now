d743b547e2c7ba91a3d064c56d14051a5bf8a588
VisvShah <visvshah1@gmail.com>
2023-01-21 16:01:52 -0500

    Finished student auth

--- front-end/src/Components/Auth/auth.jsx
+++ front-end/src/Components/Auth/auth.jsx
@@ -4,82 +4,11 @@ import TutorAuth from './tutorAuth'
 import "./auth.css"
 function Auth() {
     const [isTutor, setIsTutor] = useState(0);
-    const [logIn, changeLogIn] = useState(true);
-    const [error, setError] = useState(false);
-
-    const [loginData, setLoginData] = useState({
-        email: "",
-        password: ""
-    })
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (logIn) {
-            sendLogIn()
-        }
-        else {
-            sendSignUp()
-        }
-
-    }
-
-
-    const changeMode = () => {
-        changeLogIn(!logIn);
-    }
-
-    const sendLogIn = (e) => {
-        /** 
-        fetch("http://localhost:5001/api/users/login", { method: "POST", body: JSON.stringify(tutorData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setError(false);
-                localStorage.setItem("profile", JSON.stringify(data));
-                navigate('/');
-            })
-        .catch(e => {
-            console.log(e)
-            setError(true);
-        })
-        **/
-    }
-
-    const sendSignUp = (e) => {
-        /** 
-        fetch("http://localhost:5001/api/users/signup", { method: "POST", body: JSON.stringify(tutorData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setError(false);
-                localStorage.setItem("profile", JSON.stringify(data));
-                navigate('/');
-            })
-        .catch(e => {
-            setError(true);
-            console.log(e)
-        })
-        */
-    }
-
     return (
         <div className='authPage'>
-            <h1 className="header" >{logIn ? "Log In" : "Sign Up"}</h1>
-            {logIn && (
-                <>
-                    <input placeholder="Your email" id="email" name="email" type="email" onChange={(e) => setLoginData({ ...loginData, email: e.target.value })} />
-                    <input placeholder="Enter Password" id="password" name="password" type="password" onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
-                    <button className="submitButton" type="submit" onClick={handleSubmit}>{/*logIn ? "Next" :*/ "Submit"}</button>
-                    <button className="changeMode" onClick={changeMode}>{logIn ? "Sign Up Instead" : "Log In instead"}</button>
-                </>
-            )
-            }
-            {!logIn && (
-                <>
-                    {(isTutor === 0) && (
+            {(isTutor === 0) && (
                         <>
+                            <h1 className="logInMainHeader">What are you logging in as?</h1>
                             <button onClick={() => setIsTutor(1)} className="tutorButton">Tutor</button>
                             <button onClick={() => setIsTutor(2)} className="studentButton">Student</button>
                         </>
@@ -90,8 +19,6 @@ function Auth() {
                     {(isTutor === 2) && (
                         <StudentAuth />
                     )}
-                </>
-            )}
         </div>
     )
 }
--- front-end/src/Components/Auth/studentAuth.jsx
+++ front-end/src/Components/Auth/studentAuth.jsx
@@ -22,7 +22,6 @@ export default function StudentAuth() {
         }
     }
     const [logIn, changeLogIn] = useState(false);
-    const [error, setError] = useState(false);
     const navigate = useNavigate();
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -35,39 +34,31 @@ export default function StudentAuth() {
 
     }
     const sendLogIn = (e) => {
-        /** 
-        fetch("http://localhost:5001/api/users/login", { method: "POST", body: JSON.stringify(studentData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
+        fetch("http://localhost:5001/api/students/login", { method: "POST", body: JSON.stringify(studentData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
             .then(res => {
                 return res.json()
             })
             .then(data => {
-                setError(false);
                 localStorage.setItem("profile", JSON.stringify(data));
                 navigate('/');
             })
         .catch(e => {
             console.log(e)
-            setError(true);
         })
-        **/
     }
 
     const sendSignUp = (e) => {
-        /** 
-        fetch("http://localhost:5001/api/users/signup", { method: "POST", body: JSON.stringify(studentData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
+        fetch("http://localhost:5001/api/students/signup", { method: "POST", body: JSON.stringify(studentData), mode: 'cors', headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},contentType: "application/json"})
             .then(res => {
                 return res.json();
             })
             .then(data => {
-                setError(false);
                 localStorage.setItem("profile", JSON.stringify(data));
                 navigate('/');
             })
         .catch(e => {
-            setError(true);
             console.log(e)
         })
-        */
     }
 
     const changeMode = () => {
@@ -79,11 +70,12 @@ export default function StudentAuth() {
         fName: "",
         lName: "",
         email: "",
+        number: "",
+        venmo: "",
         password: "",
         classYear: undefined,
-        profilePicture: "",
-        interests: [],
-        classesTaken: [],
+        school: "",
+        classYear: ""
     })
 
 
@@ -92,42 +84,42 @@ export default function StudentAuth() {
             <form autoComplete="off" validate="true" className="form" onSubmit={handleSubmit}>
                 <div className="projectSlider" style={{ transform: `translateX(-${currentPage * 100}vw)` }}>
                     <div className="projectHolder">
-                        {/*<div>
-                            {!logIn && (
-                                <>
-                                <button className="studentButton">student</button>
-                                <button className="studentButton">Student</button>
-                                </>
-                            )}
-                            </div>*/}
-                        <div>
-                            {/*<h1 className="header" >{logIn ? "Sign Up" : "Log In"}</h1>*/}
-
+                        <h1 className="header" >{!logIn ? "Log In" : "Sign Up"}</h1>
+                        {logIn && (
                             <>
                                 <input placeholder="Your email" id="email" name="email" type="email" onChange={(e) => setStudentData({ ...studentData, email: e.target.value })} />
                                 <input placeholder="Enter Password" id="password" name="password" type="password" onChange={(e) => setStudentData({ ...studentData, password: e.target.value })} />
+                                <button className="submitButton" type="submit" onClick={handleSubmit}>{/*logIn ? "Next" :*/ "Submit"}</button>
+                            </>
+                        )}
+                        {!logIn && (
+                            <>
                                 <input placeholder="Your first name" id="fName" name="fName" type="fName" onChange={(e) => setStudentData({ ...studentData, fName: e.target.value })} />
                                 <input placeholder="Your last name" id="lName" name="lName" type="lName" onChange={(e) => setStudentData({ ...studentData, lName: e.target.value })} />
-                                <input placeholder="Your age" id="age" name="age" type="age" onChange={(e) => setStudentData({ ...studentData, age: e.target.value })} />
-                            </>
-                            {(currentPage > 1) && (
-                                <button onClick={() => moveLeft()} className='leftArrow'>Back</button>
-                            )
-                            }
-                            {(currentPage > 0 && currentPage != 2) && (
+                                <input placeholder="Your email" id="email" name="email" type="email" onChange={(e) => setStudentData({ ...studentData, email: e.target.value })} />
+                                <input placeholder="Enter Password" id="password" name="password" type="password" onChange={(e) => setStudentData({ ...studentData, password: e.target.value })} />
                                 <button onClick={() => moveRight()} className='submitButton'>Next</button>
-                            )
-                            }
-
-                            {error && (
-                                <>
-                                    <div className="error">Error!</div>
-                                </>
-                            )}
-                        </div>
-                        {/*<button className="changeMode" onClick={changeMode}>{logIn ? "Log In Instead" : "Sign Up instead"}</button>*/}
+                            </>
+                        )}
+                        <button className="changeMode" onClick={changeMode}>{logIn ? "Log In Instead" : "Sign Up instead"}</button>
+                    </div>
+                    <div className="projectHolder">
+                        <h1 className="header">Tell us more about yourself</h1>
+                        <input placeholder="What school do you go to?" id="school" name="school" type="school" onChange={(e) => setStudentData({ ...studentData, school: e.target.value })} />
+                        <input placeholder="What is your class standing?" id="classYear" name="classYear" type="classYear" onChange={(e) => setStudentData({ ...studentData, classYear: e.target.value })} />
+                        <button onClick={() => moveRight()} className='submitButton'>Next</button>
+                            
+                    </div>
+                    <div className="projectHolder">
+                        <h1 className="header">Tell us even more about yourself</h1>
+                        <input placeholder="What is your phone number?" id="number" name="number" onChange={(e) => setStudentData({ ...studentData, number: e.target.value })} />
+                        <input placeholder="What is your venmo?" id="venmo" name="venmo" type="venmo" onChange={(e) => setStudentData({ ...studentData, venmo: e.target.value })} />
+                        <button className="submitButton" type="submit" onClick={handleSubmit}>{/*logIn ? "Next" :*/ "Submit"}</button>
                     </div>
                 </div>
+                {(currentPage > 0) && (
+                    <button onClick={() => moveLeft()} className='leftArrow'>Back</button>
+                )}
             </form>
         </div>
     )
