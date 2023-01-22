2b54a77ae516be79964ecffda1c504ad77ca9351
VisvShah <visvshah1@gmail.com>
2023-01-22 05:22:00 -0500

    Update tutorRoutes.js

--- back-end/Routes/tutorRoutes.js
+++ back-end/Routes/tutorRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router()
-import { registerTutor, loginTutor, getTutor, getTutors} from '../Controllers/tutorController.js'
+import { registerTutor, loginTutor, getTutor, getTutors, createReview} from '../Controllers/tutorController.js'
 
 router.post('/signup', registerTutor);
 router.post('/login', loginTutor);
 router.get('/', getTutors);
 router.get('/:id', getTutor);
-
+router.patch('/review', createReview);
 export default router;
