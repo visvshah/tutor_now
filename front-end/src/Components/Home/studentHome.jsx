df2b9fdde660f372ff63a69b12b629323e604b23
Soham Jog <jogsoham2003@gmail.com>
2023-01-22 05:43:05 -0500

    Redirects

--- front-end/src/Components/Home/studentHome.jsx
+++ front-end/src/Components/Home/studentHome.jsx
@@ -79,7 +79,8 @@ export default function StudentHome({user}) {
                 return res.json()
             })
             .then(data => {
-                setFinished(true);
+                setFinished(false);
+                setDidReq(false);
                 setActualTutor(JSON.stringify(data));
             })
         .catch(e => {
